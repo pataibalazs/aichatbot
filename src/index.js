9bd68471aa932eed2bea2c02d5d15994578ebcf2
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2023-09-06 12:16:46 +0200

    openai added, fix is needed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('Hello, World!');
 });
+
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+async function runCompletion() {
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "How are you today?",
+        max_tokens: 4000
+    });
+    console.log(completion.data.choices[0].text);
+}
+runCompletion();
